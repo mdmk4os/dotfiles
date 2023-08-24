86681b1e8de261a58418ffe63add5f12358b4acc
Kaique Thomaz <68758691+mdmk4os@users.noreply.github.com>
2023-08-24 09:42:12 -0300

    fix user home

--- pos-reboot.sh
+++ pos-reboot.sh
@@ -12,58 +12,49 @@ else
     exit
 fi
 
-''' instalação
-rodar iso e configurar acesso ao teclado e internet
-zerar e configurar discos
-formatar e montar partiçoes
-instalar o sistema base com literalmente o básico para o computador funcionar e conversar com a internet
-configurações extras e pessoais, futuramente vou ativar escolhas
-configurar home/senha root
-instalar grub e aqruivos de inicialização
-ativar serviços de rede e alguns extras para o pos reboot
-
-reiniciar
-
+<<POSREBOOT
 checkar conectividade com a rede e os serviços ativos
 instalar pacotes necessários para rodar o sistema tilling manager
 adicionar usuario principal e configurar o sudo
 configurar desktop
-'''
+POSREBOOT
 
 #installing packages neededs to run your desktop
 echo -e "\nConfiguring your user:\n"
-#this pac use light and mako
-#pacman -S --needed sudo sway polkit swayidle swaylock waybar wofi git pulseaudio pulseaudio-alsa alsa-utils light mako chromium thunar
 
-#install: desktop; tools like panel, terminal, menu, browser and file-explorer; modules of audio and brightness; and fonts
-pacman -S --needed sway polkit swaybg swayidle swaylock waybar wofi kitty chromium thunar git pulseaudio pulseaudio-alsa alsa-utils brigthnessctl nerd-fonts ttf-font-awesome
+####INSTALL: desktop; tools like panel, terminal, menu, browser and file-explorer; modules of audio and brightness; and fonts
+#this pac use light and mako
+#PACKAGES="sudo sway polkit swayidle swaylock waybar wofi git pulseaudio pulseaudio-alsa alsa-utils light mako chromium thunar"
+PACKAGES="sway polkit swaybg swayidle swaylock waybar wofi kitty chromium thunar git pulseaudio pulseaudio-alsa alsa-utils brigthnessctl nerd-fonts ttf-font-awesome"
+pacman -S --needed $PACKAGES
 
 #ohmyzsh - after make other adjusts
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #Configuring user and sudo
 USERNAME="k90s"
+$USERHOME="/home/$USERNAME"
 useradd -m -G wheel,power,audio,storage,video -U $USERNAME
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 #configuring desktop with dotfiles and adjusts
-echo "cd ~" >> $USERNAME/conf-desk.sh
-echo "git clone https://github.com/mdmk4os/dotfiles.git" >> $USERNAME/conf-desk.sh
-echo "mkdir -p ~/.config/" >> $USERNAME/conf-desk.sh
+echo "cd ~" >> $USERHOME/conf-desk.sh
+echo "git clone https://github.com/mdmk4os/dotfiles.git" >> $USERHOME/conf-desk.sh
+echo "mkdir -p ~/.config/" >> $USERHOME/conf-desk.sh
 #if u want it to replace, remove the -n
-echo "mv -n ~/dotfiles/.config/* ~/.config/" >> $USERNAME/conf-desk.sh
-echo "echo -e '\nThis is ur new setup\n'" >> $USERNAME/conf-desk.sh
-echo "ls ~/.config/" >> $USERNAME/conf-desk.sh
+echo "mv -n ~/dotfiles/.config/* ~/.config/" >> $USERHOME/conf-desk.sh
+echo "echo -e '\nThis is ur new setup\n'" >> $USERHOME/conf-desk.sh
+echo "ls ~/.config/" >> $USERHOME/conf-desk.sh
 
 echo -e "\nConfiguring your desktop\n"
-su k90s -c bash $USERNAME/conf-desk.sh
+su k90s -c bash $USERHOME/conf-desk.sh
 
 echo -e "\nSetting to start sway on login.\n"
 mv ~/.bash_profile ~/.bash_profile.bkp
 echo -e "if [[ -z \$DISPLAY ]] && [[ $(tty) = /dev/tty1 ]];then\n    XKB_DEFAULT_LAYOUT=br exec sway\nfi" > $USERNAME/.bash_profile
 
 echo -e "\nRemoving temp files\n"
-rm -rf $USERNAME/dotfiles
-rm -rf $USERNAME/.bash_profile.bkp
+rm -rf $USERHOME/dotfiles
+rm -rf $USERHOME/.bash_profile.bkp
 
 echo -e "\n!! Complete !!\n"
